80d59fbc2dbf4a76cb019dd249038053fafc0608
MacModeler <memcbride@mac.com>
2024-06-29 14:33:28 -0400

    Initial Commit

---
+++ ProgressView/ProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewApp.swift
+//  ProgressView
+//
+//  Created by Mark McBride on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
